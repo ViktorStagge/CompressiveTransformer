b1be408abc7fe91ec1db47d19c7c988b9c3e001f
Viktor Stagge <viktorstagge@gmail.com>
2020-07-07 23:59:10 +0200

    fix: remote-sync

---
+++ ct/model/layers/embedding.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+from keras import layers
+from keras import activations
+from keras import backend as K
+from keras.layers import Layer
+from omegaconf import OmegaConf
+
+from model.utils import cosine_similarity
+
+
+class ReverseEmbedding(Layer):
+    def __init__(self, embedding_layer, activation=None, **kwargs):
+        super().__init__(**kwargs)
+        self.embedding_layer = embedding_layer
+        self.activation = activations.get(activation)
+        self.vocab_size = embedding_layer.get_config()['input_dim']
+        self.trainable = False
+
+    def build(self, input_shape):
+        super().build(input_shape)
+
+    def call(self, inputs, **kwargs):
+        assert len(inputs.shape) == 3, \
+            'expected 3 dimensions'
+
+        input_emb = inputs[:, -1, :]
+        w_transpose = K.transpose(self.embedding_layer.embeddings)  # OK with w_tranpose?
+
+        y = K.dot(input_emb, w_transpose)
+        # if self.activation == 'softmax':
+        #     y = K.softmax(y, axis=-1)
+        if self.activation is not None:
+            y = self.activation(y)
+        return y
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[0], self.embedding_layer.embeddings.shape[0]
